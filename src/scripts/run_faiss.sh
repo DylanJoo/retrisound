c3d20f9ccbc24b7695fbbf0fd9f797f8b7ebd3df
DylanJoo <dylanjootw@gmail.com>
2024-08-16 15:48:43 +0200

    revise encoding sript

--- src/scripts/run_faiss.sh
+++ src/scripts/run_faiss.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:nvidia_rtx_a6000:1
 #SBATCH --mem=32G
 #SBATCH --nodes=1
-#SBATCH --array=1-4%4
+#SBATCH --array=1-11%4
 #SBATCH --time=72:00:00
 #SBATCH --output=logs/%x-%j.out
 
@@ -20,11 +20,9 @@ python -m pyserini.encode input \
     --corpus ${DATASET_DIR}/wikipedia_split/raw/psgs_w100.jsonl \
     --fields text \
     $(head -$SLURM_ARRAY_TASK_ID $HPARAMS_FILE | tail -1) \
-    --delimiter "\n" output \
-    --embeddings ${INDEX_DIR}/wikipedia_split/contriever.psgs_w100.faiss \
     --to-faiss encoder \
     --encoder facebook/contriever-msmarco \
     --encoder-class contriever \
     --fields text \
-    --batch 64 \
+    --batch 128 \
     --fp16  
